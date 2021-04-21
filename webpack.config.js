5981cfb961d1200e8e1bbe07102ee4149d201d5d
fakundo <fakundo.roman@gmail.com>
2021-04-21 12:53:05 +0500

    Update sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -2,103 +2,9 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { default: SitemapWebpackPlugin } = require('sitemap-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const links = require('./links')
 
 const publicPath = 'https://fakundo.github.io/'
-
-const links = [
-  {
-    href: 'https://fakundo.github.io/tarkov-raid-items/',
-    title: 'Escape from Tarkov - [EFT] Interactive Find in Raid Items List',
-  },
-  {
-    href: 'https://github.com/fakundo/tarkov-raid-items',
-    title: '[SOURCE] Escape from Tarkov - [EFT] Interactive Find in Raid Items List',
-  },
-  {
-    href: 'https://github.com/fakundo/valheim-server',
-    title: '[SOURCE] Valheim Dedicated Server with Web Interface exposed in Docker image',
-  },
-  {
-    href: 'https://hub.docker.com/r/fakundo/valheim-server',
-    title: '[DOCKER HUB] Valheim Dedicated Server with Web Interface exposed in Docker image',
-  },
-  {
-    href: 'https://github.com/fakundo/go-middleware',
-    title: '[SOURCE] Middleware for Go functions, useful for socket.io event handlers',
-  },
-  {
-    href: 'https://fakundo.github.io/preact-transitioning/',
-    title: '[DEMO] Preact transition components',
-  },
-  {
-    href: 'https://github.com/fakundo/preact-transitioning',
-    title: '[SOURCE] Preact transition components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/preact-transitioning',
-    title: '[NPM PACKAGE] Preact transition components',
-  },
-  {
-    href: 'https://github.com/fakundo/preact-jss-hook',
-    title: '[SOURCE] Preact integration with JSS',
-  },
-  {
-    href: 'https://www.npmjs.com/package/preact-jss-hook',
-    title: '[NPM PACKAGE] Preact integration with JSS',
-  },
-  {
-    href: 'https://github.com/fakundo/react-vld',
-    title: '[SOURCE] Validation for React & Preact components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/react-vld',
-    title: '[NPM PACKAGE] Validation for React components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/preact-vld',
-    title: '[NPM PACKAGE] Validation for Preact components',
-  },
-  {
-    href: 'https://fakundo.github.io/react-input-focus/react/',
-    title: '[DEMO] Focus switcher for React & Preact input components',
-  },
-  {
-    href: 'https://github.com/fakundo/react-input-focus',
-    title: '[SOURCE] Focus switcher for React & Preact input components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/react-input-focus',
-    title: '[NPM PACKAGE] Focus switcher for React input components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/preact-input-focus',
-    title: '[NPM PACKAGE] Focus switcher for Preact input components',
-  },
-  {
-    href: 'https://github.com/fakundo/react-redux-getters',
-    title: '[SOURCE] Getters for React and Redux',
-  },
-  {
-    href: 'https://www.npmjs.com/package/react-redux-getters',
-    title: '[NPM PACKAGE] Getters for React and Redux',
-  },
-  {
-    href: 'https://fakundo.github.io/react-localized/',
-    title: '[DEMO] Internationalization for React & Preact components',
-  },
-  {
-    href: 'https://github.com/fakundo/react-localized',
-    title: '[SOURCE] Internationalization for React & Preact components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/react-localized',
-    title: '[NPM PACKAGE] Internationalization for React components',
-  },
-  {
-    href: 'https://www.npmjs.com/package/preact-localized',
-    title: '[NPM PACKAGE] Internationalization for Preact components',
-  },
-]
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index'),
@@ -123,9 +29,13 @@ module.exports = {
     new CleanWebpackPlugin(),
     new SitemapWebpackPlugin({
       base: publicPath,
-      paths: links.map((link) => ({
-        path: link.href,
-      })),
+      paths: links
+        .filter((link) => (
+          link.href.indexOf(publicPath) === 0
+        ))
+        .map((link) => ({
+          path: link.href,
+        })),
       options: {
         skipgzip: true,
       },
