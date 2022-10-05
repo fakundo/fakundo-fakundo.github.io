3099ac51737e7cec20d5f81c6c98007b9ef5c246
fakundo <fakundo.roman@gmail.com>
2022-10-05 23:33:37 +0500

    Update links

--- links.js
+++ links.js
@@ -1,102 +1,114 @@
 module.exports = [
   {
-    href: 'https://fakundo.github.io/tarkov-raid-items/',
-    title: 'Escape from Tarkov - [EFT] Interactive Find in Raid Items List',
+    href: "https://fakundo.github.io/tarkov-raid-items/",
+    title: "Escape from Tarkov - [EFT] Interactive Find in Raid Items List",
   },
   {
-    href: 'https://github.com/fakundo/tarkov-raid-items',
-    title: '[SOURCE] Escape from Tarkov - [EFT] Interactive Find in Raid Items List',
+    href: "https://github.com/fakundo/tarkov-raid-items",
+    title:
+      "[SOURCE] Escape from Tarkov - [EFT] Interactive Find in Raid Items List",
   },
   {
-    href: 'https://github.com/fakundo/valheim-server',
-    title: '[SOURCE] Valheim Dedicated Server with Web Interface exposed in Docker image',
+    href: "https://github.com/fakundo/valheim-server",
+    title:
+      "[SOURCE] Valheim Dedicated Server with Web Interface exposed in Docker image",
   },
   {
-    href: 'https://hub.docker.com/r/fakundo/valheim-server',
-    title: '[DOCKER HUB] Valheim Dedicated Server with Web Interface exposed in Docker image',
+    href: "https://hub.docker.com/r/fakundo/valheim-server",
+    title:
+      "[DOCKER HUB] Valheim Dedicated Server with Web Interface exposed in Docker image",
   },
   {
-    href: 'https://github.com/fakundo/go-middleware',
-    title: '[SOURCE] Middleware for Go functions, useful for socket.io event handlers',
+    href: "https://github.com/fakundo/go-middleware",
+    title:
+      "[SOURCE] Middleware for Go functions, useful for socket.io event handlers",
   },
   {
-    href: 'https://fakundo.github.io/preact-transitioning/',
-    title: '[DEMO] Preact transition components',
+    href: "https://fakundo.github.io/preact-transitioning/",
+    title: "[DEMO] Preact transition components",
   },
   {
-    href: 'https://github.com/fakundo/preact-transitioning',
-    title: '[SOURCE] Preact transition components',
+    href: "https://github.com/fakundo/preact-transitioning",
+    title: "[SOURCE] Preact transition components",
   },
   {
-    href: 'https://www.npmjs.com/package/preact-transitioning',
-    title: '[NPM PACKAGE] Preact transition components',
+    href: "https://www.npmjs.com/package/preact-transitioning",
+    title: "[NPM PACKAGE] Preact transition components",
   },
   {
-    href: 'https://github.com/fakundo/preact-jss-hook',
-    title: '[SOURCE] Preact integration with JSS',
+    href: "https://github.com/fakundo/preact-jss-hook",
+    title: "[SOURCE] Preact integration with JSS",
   },
   {
-    href: 'https://www.npmjs.com/package/preact-jss-hook',
-    title: '[NPM PACKAGE] Preact integration with JSS',
+    href: "https://www.npmjs.com/package/preact-jss-hook",
+    title: "[NPM PACKAGE] Preact integration with JSS",
   },
   {
-    href: 'https://github.com/fakundo/react-vld',
-    title: '[SOURCE] Validation for React & Preact components',
+    href: "https://github.com/fakundo/react-vld",
+    title: "[SOURCE] Validation for React & Preact components",
   },
   {
-    href: 'https://www.npmjs.com/package/react-vld',
-    title: '[NPM PACKAGE] Validation for React components',
+    href: "https://www.npmjs.com/package/react-vld",
+    title: "[NPM PACKAGE] Validation for React components",
   },
   {
-    href: 'https://www.npmjs.com/package/preact-vld',
-    title: '[NPM PACKAGE] Validation for Preact components',
+    href: "https://www.npmjs.com/package/preact-vld",
+    title: "[NPM PACKAGE] Validation for Preact components",
   },
   {
-    href: 'https://fakundo.github.io/react-input-focus/react/',
-    title: '[DEMO] Focus switcher for React & Preact input components',
+    href: "https://fakundo.github.io/react-input-focus/react/",
+    title: "[DEMO] Focus switcher for React & Preact input components",
   },
   {
-    href: 'https://github.com/fakundo/react-input-focus',
-    title: '[SOURCE] Focus switcher for React & Preact input components',
+    href: "https://github.com/fakundo/react-input-focus",
+    title: "[SOURCE] Focus switcher for React & Preact input components",
   },
   {
-    href: 'https://www.npmjs.com/package/react-input-focus',
-    title: '[NPM PACKAGE] Focus switcher for React input components',
+    href: "https://www.npmjs.com/package/react-input-focus",
+    title: "[NPM PACKAGE] Focus switcher for React input components",
   },
   {
-    href: 'https://www.npmjs.com/package/preact-input-focus',
-    title: '[NPM PACKAGE] Focus switcher for Preact input components',
+    href: "https://www.npmjs.com/package/preact-input-focus",
+    title: "[NPM PACKAGE] Focus switcher for Preact input components",
   },
   {
-    href: 'https://github.com/fakundo/react-redux-getters',
-    title: '[SOURCE] Getters for React and Redux',
+    href: "https://github.com/fakundo/react-redux-getters",
+    title: "[SOURCE] Getters for React and Redux",
   },
   {
-    href: 'https://www.npmjs.com/package/react-redux-getters',
-    title: '[NPM PACKAGE] Getters for React and Redux',
+    href: "https://www.npmjs.com/package/react-redux-getters",
+    title: "[NPM PACKAGE] Getters for React and Redux",
   },
   {
-    href: 'https://fakundo.github.io/react-localized/',
-    title: '[DEMO] Internationalization for React & Preact components',
+    href: "https://fakundo.github.io/react-localized/",
+    title: "[DEMO] Internationalization for React & Preact components",
   },
   {
-    href: 'https://github.com/fakundo/react-localized',
-    title: '[SOURCE] Internationalization for React & Preact components',
+    href: "https://github.com/fakundo/react-localized",
+    title: "[SOURCE] Internationalization for React & Preact components",
   },
   {
-    href: 'https://www.npmjs.com/package/react-localized',
-    title: '[NPM PACKAGE] Internationalization for React components',
+    href: "https://www.npmjs.com/package/react-localized",
+    title: "[NPM PACKAGE] Internationalization for React components",
   },
   {
-    href: 'https://www.npmjs.com/package/preact-localized',
-    title: '[NPM PACKAGE] Internationalization for Preact components',
+    href: "https://www.npmjs.com/package/preact-localized",
+    title: "[NPM PACKAGE] Internationalization for Preact components",
   },
   {
-    href: 'https://fakundo.github.io/webgl-audio/',
-    title: '[DEMO] WebGL audio visualisation experiment',
+    href: "https://fakundo.github.io/webgl-audio/",
+    title: "[DEMO] WebGL audio visualisation experiment",
   },
   {
-    href: 'https://github.com/fakundo/webgl-audio',
-    title: '[SOURCE] WebGL audio visualisation experiment',
+    href: "https://github.com/fakundo/webgl-audio",
+    title: "[SOURCE] WebGL audio visualisation experiment",
   },
-]
+  {
+    href: "https://fakundo.github.io/next-redux-store/",
+    title: "[DEMO] Next integration with Redux",
+  },
+  {
+    href: "https://github.com/fakundo/next-redux-store",
+    title: "[SOURCE] Next integration with Redux",
+  },
+];
